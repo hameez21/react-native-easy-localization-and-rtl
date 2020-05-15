7b56d67c7f07850c97b3fe1a439d8c764882c7ec
Hameez Rehman <hameez21@live.com>
2020-05-15 21:37:47 +0500

    commit

--- README.m
+++
@@ -1,6 +0,0 @@
-React Native Easy Localization and RTL
-This library is for creating multi-Language supported apps easily on android and ios using react native.
-It helps to change language and layout easily using predefined styles. 
-
-Getting Started
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
